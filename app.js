15f19ff73fb72eac61854b7f0309a5881c106736
Court Cox <82842603+coxco96@users.noreply.github.com>
2022-09-08 14:29:49 -0400

    update commit method

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ function filesAndCharts(countyObj, location) {
 
     // create new subfolder in 'csvs/' if one does not already exist for location
     if (!fs.existsSync(location.fileID)) {
-        fs.mkdir(`affordability-dashboard/csvs/${location.fileID}`, (err) => {
+        fs.mkdir(`/csvs/${location.fileID}`, (err) => {
             if (err) return err;
         })
     }
@@ -110,11 +110,11 @@ function filesAndCharts(countyObj, location) {
             });
 
             // write csv
-            fs.writeFile(`affordability-dashboard/csvs/${location.fileID}/${location.fileID}-${key}.csv`, unparsed, (err) => {
+            fs.writeFile(`/csvs/${location.fileID}/${location.fileID}-${key}.csv`, unparsed, (err) => {
                 if (err) {
                     console.log(err)
                 } else {
-                    console.log(`affordability-dashboard/csvs/${location.fileID}/${location.fileID}-${key}.csv written successfully`)
+                    console.log(`/csvs/${location.fileID}/${location.fileID}-${key}.csv written successfully`)
                 }
             });
 
